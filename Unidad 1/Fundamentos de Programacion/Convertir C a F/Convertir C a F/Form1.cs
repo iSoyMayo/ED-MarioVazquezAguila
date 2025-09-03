302db2aab9df7fe2498618785744332c8648ca7c
iSoyMayo <mv50370@gmail.com>
2025-09-03 00:26:50 -0500

    Convertir Grados F

---
+++ Unidad 1/Fundamentos de Programacion/Convertir C a F/Convertir C a F/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Convertir_C_a_F
+{
+    public partial class frmInicio: Form
+    {
+        public frmInicio()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnConvertir_Click(object sender, EventArgs e)
+        {
+            int Resultado;
+
+            Resultado = (int.Parse(txtGradosC.Text) * 9/5)+32;
+
+            txtGradosF.Text = Resultado.ToString();
+        }
+    }
+}

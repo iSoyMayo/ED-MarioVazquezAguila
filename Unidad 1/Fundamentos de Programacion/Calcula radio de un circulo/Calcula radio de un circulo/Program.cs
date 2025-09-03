81f41802906614259bbd619b7b8d227399c0613b
iSoyMayo <mv50370@gmail.com>
2025-09-03 00:47:04 -0500

    Calcular Area circulo

---
+++ Unidad 1/Fundamentos de Programacion/Calcula radio de un circulo/Calcula radio de un circulo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calcula_radio_de_un_circulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double  RadioCirculo;
+            double Resultado;
+
+            Console.Write("> Ingrese el radio del cuadrado: ");
+            RadioCirculo = int.Parse(Console.ReadLine());
+
+            Resultado = Math.PI * Math.Pow(RadioCirculo, 2);
+
+            Console.WriteLine($"\n> El resultado es: {Resultado}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
